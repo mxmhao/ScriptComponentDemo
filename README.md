1b1088acbaa1049f406f747b4d5a7375006ae22e
maoxm <243559086@qq.com>
2023-03-14 08:57:10 +0800

    加入readme文件，去掉log

---
+++ README.md
@@ -0,0 +1,2 @@
+# ScriptComponentDemo
+此项目是一个模块自动拼装示例，通过[脚本](/ThirdPartyService/Classes/Exclude/build.sh)把类似插件的子模块，自动引入到特定的头文件中，并把头文件放到Pods/Headers/Public目录下，方便直接引入。
--- ThirdPartyService/Classes/Exclude/build.sh
+++ ThirdPartyService/Classes/Exclude/build.sh
@@ -5,7 +5,7 @@ echo "pwd : "`pwd` >> ../log.txt
 
 # 获取有效的pod依赖库
 podsArr=(`grep "^[^#]pod" ../Podfile | sed 's/[[:space:]]//g' | awk -F "'|\"" '{print $2}'`)
-echo "podsArr : "$podsArr >> ../log.txt
+#echo "podsArr : "$podsArr >> ../log.txt
 
 if ! test -f "ThirdPartyService/Classes/Exclude/build.sh" ; then
     cd ..
@@ -17,18 +17,18 @@ fi
 
 moduleProtocolFiles=(`grep "@ThirdPartyService" -rl --include='*.h' . | awk -F '/' '{print $NF}'`)
 
-echo "moduleProtocolFiles : "$moduleProtocolFiles >> log.txt
+#echo "moduleProtocolFiles : "$moduleProtocolFiles >> log.txt
 
-echo "sh : "$0 >> log.txt
-echo "SRCROOT : "${SRCROOT} >> log.txt
-echo "PODS_ROOT : "${PODS_ROOT} >> log.txt
+#echo "sh : "$0 >> log.txt
+#echo "SRCROOT : "${SRCROOT} >> log.txt
+#echo "PODS_ROOT : "${PODS_ROOT} >> log.txt
 #echo "\n\n" >> ../log.txt
 # 从 set.txt 里面找所需目录的全局变量
-cat /dev/null > "set.txt"
-echo `set` >> "set.txt"
+#cat /dev/null > "set.txt"
+#echo `set` >> "set.txt"
 
 #cd ThirdPartyService/Classes
-cd Pods/Headers
+cd Pods/Headers/Public
 
 ThirdPartyProtocolPath=ThirdPartyProtocol.h
 
